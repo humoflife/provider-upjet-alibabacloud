89a2dd10c9aa239e4a1e2e9329fe3d7f46abc26b
humoflife <markus.schweig@upbound.io>
2025-03-31 16:06:19 -0700

    introduce const per linter
    Signed-off-by: humoflife <markus.schweig@upbound.io>

--- config/ecs/config.go
+++ config/ecs/config.go
@@ -2,106 +2,108 @@ package ecs
 
 import "github.com/crossplane/upjet/pkg/config"
 
+const Ecs = "ecs"
+
 // Configure configures individual resources by adding custom ResourceConfigurators.
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("alicloud_auto_provisioning_group", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
 		// this resource, which would be "ecs"
 		r.Kind = "AutoProvisioningGroup"
-		r.ShortGroup = "ecs"
+		r.ShortGroup = Ecs
 	})
 	p.AddResourceConfigurator("alicloud_disk", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "ecs"
-		r.ShortGroup = "ecs"
+		// this resource, which would be Ecs
+		r.ShortGroup = Ecs
 	})
 	p.AddResourceConfigurator("alicloud_disk_attachment", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "ecs"
+		// this resource, which would be Ecs
 		r.Kind = "DiskAttachment"
-		r.ShortGroup = "ecs"
+		r.ShortGroup = Ecs
 	})
 	p.AddResourceConfigurator("alicloud_image", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "ecs"
-		r.ShortGroup = "ecs"
+		// this resource, which would be Ecs
+		r.ShortGroup = Ecs
 	})
 	p.AddResourceConfigurator("alicloud_image_copy", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "ecs"
+		// this resource, which would be Ecs
 		r.Kind = "ImageCopy"
-		r.ShortGroup = "ecs"
+		r.ShortGroup = Ecs
 	})
 	p.AddResourceConfigurator("alicloud_image_export", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "ecs"
+		// this resource, which would be Ecs
 		r.Kind = "ImageExport"
-		r.ShortGroup = "ecs"
+		r.ShortGroup = Ecs
 	})
 	p.AddResourceConfigurator("alicloud_image_import", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "ecs"
+		// this resource, which would be Ecs
 		r.Kind = "ImageImport"
-		r.ShortGroup = "ecs"
+		r.ShortGroup = Ecs
 	})
 	p.AddResourceConfigurator("alicloud_image_share_permission", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "ecs"
+		// this resource, which would be Ecs
 		r.Kind = "ImageSharePermission"
-		r.ShortGroup = "ecs"
+		r.ShortGroup = Ecs
 	})
 	p.AddResourceConfigurator("alicloud_instance", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "ecs"
-		r.ShortGroup = "ecs"
+		// this resource, which would be Ecs
+		r.ShortGroup = Ecs
 	})
 	p.AddResourceConfigurator("alicloud_key_pair", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "ecs"
+		// this resource, which would be Ecs
 		r.Kind = "KeyPair"
-		r.ShortGroup = "ecs"
+		r.ShortGroup = Ecs
 	})
 	p.AddResourceConfigurator("alicloud_key_pair_attachment", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "ecs"
+		// this resource, which would be Ecs
 		r.Kind = "KeyPairAttachment"
-		r.ShortGroup = "ecs"
+		r.ShortGroup = Ecs
 	})
 	p.AddResourceConfigurator("alicloud_launch_template", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "ecs"
+		// this resource, which would be Ecs
 		r.Kind = "LaunchTemplate"
-		r.ShortGroup = "ecs"
+		r.ShortGroup = Ecs
 	})
 	p.AddResourceConfigurator("alicloud_network_interface", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "ecs"
+		// this resource, which would be Ecs
 		r.Kind = "NetworkInterface"
-		r.ShortGroup = "ecs"
+		r.ShortGroup = Ecs
 	})
 	p.AddResourceConfigurator("alicloud_network_interface_attachment", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "ecs"
+		// this resource, which would be Ecs
 		r.Kind = "NetworkInterfaceAttachment"
-		r.ShortGroup = "ecs"
+		r.ShortGroup = Ecs
 	})
 	p.AddResourceConfigurator("alicloud_ram_role_attachment", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "ecs"
+		// this resource, which would be Ecs
 		r.Kind = "RamRoleAttachment"
-		r.ShortGroup = "ecs"
+		r.ShortGroup = Ecs
 	})
 	p.AddResourceConfigurator("alicloud_reserved_instance", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "ecs"
+		// this resource, which would be Ecs
 		r.Kind = "ReservedInstance"
-		r.ShortGroup = "ecs"
+		r.ShortGroup = Ecs
 	})
 	p.AddResourceConfigurator("alicloud_security_group", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "ecs"
+		// this resource, which would be Ecs
 		r.Kind = "SecurityGroup"
-		r.ShortGroup = "ecs"
+		r.ShortGroup = Ecs
 		r.References["vpc_id"] = config.Reference{
 			TerraformName: "alicloud_vpc",
 		}
@@ -114,19 +116,19 @@ func Configure(p *config.Provider) {
 	})
 	p.AddResourceConfigurator("alicloud_security_group_rule", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "ecs"
+		// this resource, which would be Ecs
 		r.Kind = "SecurityGroupRule"
-		r.ShortGroup = "ecs"
+		r.ShortGroup = Ecs
 	})
 	p.AddResourceConfigurator("alicloud_snapshot", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "ecs"
-		r.ShortGroup = "ecs"
+		// this resource, which would be Ecs
+		r.ShortGroup = Ecs
 	})
 	p.AddResourceConfigurator("alicloud_snapshot_policy", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "ecs"
+		// this resource, which would be Ecs
 		r.Kind = "SnapshotPolicy"
-		r.ShortGroup = "ecs"
+		r.ShortGroup = Ecs
 	})
 }
--- config/vpc/config.go
+++ config/vpc/config.go
@@ -5,12 +5,14 @@ import (
 	"github.com/crossplane/upjet/pkg/config"
 )
 
+const Vpc = "vpc"
+
 // Configure configures individual resources by adding custom ResourceConfigurators.
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("alicloud_vpc", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
 		// this resource, which would be "github"
-		r.ShortGroup = "vpc"
+		r.ShortGroup = Vpc
 		// Delete deprecated fields
 		delete(r.TerraformResource.Schema, "name")
 		delete(r.TerraformResource.Schema, "router_table_id")
@@ -18,51 +20,51 @@ func Configure(p *config.Provider) {
 	})
 	p.AddResourceConfigurator("alicloud_havip", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "vpc"
-		r.ShortGroup = "vpc"
+		// this resource, which would be Vpc
+		r.ShortGroup = Vpc
 
 		delete(r.TerraformResource.Schema, "havip_id")
 	})
 	p.AddResourceConfigurator("alicloud_haveip_attachment", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "vpc"
+		// this resource, which would be Vpc
 		r.Kind = "HaveIpAttachment"
-		r.ShortGroup = "vpc"
+		r.ShortGroup = Vpc
 	})
 	p.AddResourceConfigurator("alicloud_network_acl", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "vpc"
-		r.ShortGroup = "vpc"
+		// this resource, which would be Vpc
+		r.ShortGroup = Vpc
 	})
 	p.AddResourceConfigurator("alicloud_network_acl_attachment", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "vpc"
-		r.ShortGroup = "vpc"
+		// this resource, which would be Vpc
+		r.ShortGroup = Vpc
 	})
 	p.AddResourceConfigurator("alicloud_network_acl_entries", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "vpc"
-		r.ShortGroup = "vpc"
+		// this resource, which would be Vpc
+		r.ShortGroup = Vpc
 	})
 	p.AddResourceConfigurator("alicloud_route_entry", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "vpc"
-		r.ShortGroup = "vpc"
+		// this resource, which would be Vpc
+		r.ShortGroup = Vpc
 	})
 	p.AddResourceConfigurator("alicloud_route_table", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "vpc"
-		r.ShortGroup = "vpc"
+		// this resource, which would be Vpc
+		r.ShortGroup = Vpc
 	})
 	p.AddResourceConfigurator("alicloud_route_table_attachment", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "vpc"
-		r.ShortGroup = "vpc"
+		// this resource, which would be Vpc
+		r.ShortGroup = Vpc
 	})
 	p.AddResourceConfigurator("alicloud_vswitch", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		// this resource, which would be "vpc"
-		r.ShortGroup = "vpc"
+		// this resource, which would be Vpc
+		r.ShortGroup = Vpc
 		r.References["vpc_id"] = config.Reference{
 			TerraformName: "alicloud_vpc",
 			Extractor:     common.PathIdExtractor,
